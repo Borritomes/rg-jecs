adee08593e86614de99177e42d1d6245f2fc3323
Borritomes <4611hd@gmail.com>
2025-04-30 17:06:14 -0400

    more setup

--- Packages/YetAnotherNet.lua
+++ Packages/YetAnotherNet.lua
@@ -1,1 +1,8 @@
-return require(script.Parent._Index["yetanotherclown_yetanothernet@0.10.0-alpha.5"]["yetanothernet"])
+local REQUIRED_MODULE = require(script.Parent._Index["yetanotherclown_yetanothernet@0.10.0-alpha.5"]["yetanothernet"])
+export type Identifier = REQUIRED_MODULE.Identifier 
+export type QueryResult<U...> = REQUIRED_MODULE.QueryResult<U...>
+export type SendRequest = REQUIRED_MODULE.SendRequest 
+export type Connection = REQUIRED_MODULE.Connection 
+export type Configuration = REQUIRED_MODULE.Configuration 
+export type Route<U...> = REQUIRED_MODULE.Route<U...>
+return REQUIRED_MODULE
