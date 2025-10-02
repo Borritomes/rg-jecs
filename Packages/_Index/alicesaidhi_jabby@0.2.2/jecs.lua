0bcc77dfe0d0aa9b1b37faea6d5d2147294deca0
Borritomes <4611hd@gmail.com>
2025-10-02 16:23:06 -0400

    a

--- Packages/_Index/alicesaidhi_jabby@0.2.2/jecs.lua
+++ Packages/_Index/alicesaidhi_jabby@0.2.2/jecs.lua
@@ -1,11 +1,1 @@
-local REQUIRED_MODULE = require(script.Parent.Parent["ukendio_jecs@0.5.5"]["jecs"])
-export type Archetype = REQUIRED_MODULE.Archetype 
-export type Record = REQUIRED_MODULE.Record 
-export type Entity<T > = REQUIRED_MODULE.Entity<T >
-export type Id<T > =
-REQUIRED_MODULE.Id<T >
-export type Pair<P, O> = REQUIRED_MODULE.Pair<P, O>
-export type Query<T...> = REQUIRED_MODULE.Query<T...>
-export type Observer = REQUIRED_MODULE.Observer 
-export type World = REQUIRED_MODULE.World 
-return REQUIRED_MODULE
+return require(script.Parent.Parent["ukendio_jecs@0.5.5"]["jecs"])
